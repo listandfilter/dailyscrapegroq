8ad6292ae4d157e8524366199053a08060941fa0
Mimansa Sahay <mimansa192@gmail.com>
2025-06-09 22:06:36 +0530

    uodate feeddailygainer

--- feeddailygainerA.js
+++ feeddailygainerA.js
@@ -14,7 +14,7 @@ async function scrapeStockFeeds() {
 
   console.log('Starting browser...');
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
    
     defaultViewport: null,
     timeout: 0,
