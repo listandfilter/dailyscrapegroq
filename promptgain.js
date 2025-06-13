bd33841fb2a73fd86b076972a149fa589a48751b
Mimansa Sahay <mimansa192@gmail.com>
2025-06-14 00:19:19 +0530

    updated prompt

--- promptgain.js
+++ promptgain.js
@@ -121,8 +121,8 @@ async function getCompanyFeed(stockName) {
 
 async function classifyFeedWithGroq(stockName, feed, apikey) {
   const prompt = `You are a financial analyst. Analyze the following recent news and updates about the NSE-listed company "${stockName}":${feed}
-From this feed, identify and give the **top 3 feed** why this company may be appearing as a monthly loser in the stock market. Base your reasoning only on the feed content. Be concise and specific. 
-From the above content, Find 3 reasons explaining the monthly loser behaviour, and give each reason in 100 characters with a title of 2 words for each reason.
+From this feed, identify and give the **top 3 feed** why this company may be appearing as a monthly gainer in the stock market. Base your reasoning only on the feed content. Be concise and specific. 
+From the above content, Find 3 reasons explaining the monthly gainer behaviour, and give each reason in 100 characters with a title of 2 words for each reason.
 Format:
 1. Title: Explanation (up to 100 characters)
 2. Title: Explanation
@@ -218,3 +218,5 @@ export async function rundailygain() {
     await delay(30000); 
   }
 }
+
+rundailygain()
